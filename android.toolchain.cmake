1a4d917026ec1bafaf25d24f80b3b9a3b019492c
Benn Herrera <bherrera@reification.io>
2018-09-20 18:40:03 -0700

    added support for iterative dev on ReifyOmnibus via install prefix. hard-specified android toolchain config vars.

--- android.toolchain.cmake
+++ android.toolchain.cmake
@@ -1,3 +1,8 @@
 #include the toolchain file for generating visual studio projects using mobile c++ android workflow
 #see https://github.com/Reification/CMake/releases if you want to generate for MSVS android c++ workflow
+
+set(ANDROID_ABI arm64-v8a)
+set(ANDROID_NATIVE_API_LEVEL 26)
+set(ANDROID_TOOLCHAIN clang)
+
 include("${CMAKE_ROOT}/msvs-android/android.toolchain.cmake")
